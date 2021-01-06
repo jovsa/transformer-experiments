3c3147b16b233a917b62a524ac140a7bc1fde42a
Jovan Sardinha <jovan.sardinha@gmail.com>
2021-01-06 13:42:37 -0800

    temp code for debugging why tensor is None

--- main.py
+++ main.py
@@ -13,18 +13,67 @@ def data_gen(V, batch, nbatches):
         yield Batch(src, tgt, 0)
 
 
+def loss_backprop(generator, criterion, out, targets, normalize):
+    """
+    Memory optmization. Compute each timestep separately and sum grads.
+    """
+    assert out.size(1) == targets.size(1)
+    total = 0.0
+    out_grad = []
+    for i in range(out.size(1)):
+        out_column = Variable(out[:, i].data, requires_grad=True)
+        gen = generator(out_column)
+        loss = criterion(gen, targets[:, i]) / normalize
+        total += loss.data[0]
+        loss.backward()
+        out_grad.append(out_column.grad.data.clone())
+    out_grad = torch.stack(out_grad, dim=1)
+    out.backward(gradient=out_grad)
+    return total
+
+
+def make_std_mask(src, tgt, pad):
+    src_mask = (src != pad).unsqueeze(-2)
+    tgt_mask = (tgt != pad).unsqueeze(-2)
+    tgt_mask = tgt_mask & Variable(subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+    return src_mask, tgt_mask
+
+
+def train_epoch(train_iter, model, criterion, opt, transpose=False):
+    model.train()
+    for i, batch in enumerate(train_iter):
+        src, trg, src_mask, trg_mask = \
+            batch.src, batch.trg, batch.src_mask, batch.trg_mask
+        out = model.forward(src, trg[:, :-1], src_mask, trg_mask[:, :-1, :-1])
+        loss = loss_backprop(model.generator, criterion, out, trg[:, 1:], batch.ntokens)
+
+        model_opt.step()
+        model_opt.optimizer.zero_grad()
+        if i % 10 == 1:
+            print(i, loss, model_opt._rate)
+
+
 ## Greedy Decoding
 # Train the simple copy task.
+
+### old
+# V = 11
+# criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
+# model = make_model(V, V, N=2)
+# model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
+#         torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+
+# for epoch in range(10):
+#     model.train()
+#     run_epoch(data_gen(V, 30, 20), model,
+#               SimpleLossCompute(model.generator, criterion, model_opt))
+#     model.eval()
+#     print(run_epoch(data_gen(V, 30, 5), model,
+#                     SimpleLossCompute(model.generator, criterion, None)))
+
 V = 11
 criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
 model = make_model(V, V, N=2)
-model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
-        torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
-
-for epoch in range(10):
-    model.train()
-    run_epoch(data_gen(V, 30, 20), model,
-              SimpleLossCompute(model.generator, criterion, model_opt))
-    model.eval()
-    print(run_epoch(data_gen(V, 30, 5), model,
-                    SimpleLossCompute(model.generator, criterion, None)))
+model_opt = get_std_opt(model)
+for epoch in range(2):
+    train_epoch(data_gen(V, 30, 20), model, criterion, model_opt)
