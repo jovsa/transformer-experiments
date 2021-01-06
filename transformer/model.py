079f793ec4873fe52edc5cdb09402f2023a8c7d5
Jovan Sardinha <jovan.sardinha@gmail.com>
2021-01-06 13:39:49 -0800

    updated xavier_uniform function

--- transformer/model.py
+++ transformer/model.py
@@ -252,5 +252,5 @@ def make_model(src_vocab, tgt_vocab, N=6,
     # Initialize parameters with Glorot / fan_avg.
     for p in model.parameters():
         if p.dim() > 1:
-            nn.init.xavier_uniform(p)
+            nn.init.xavier_uniform_(p)
     return model
